334f731b8943421db0401230e5e8bfa79d1a4703
Rafalmilevski <168198043+Rafalmilevski@users.noreply.github.com>
2026-05-08 18:20:52 +0300

    Set up Express server with a rank endpoint

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
 
+app.get("/rank", (req, res) => {
+    res.json({ player: "Test", rank: 99 });
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Server running");
 });
