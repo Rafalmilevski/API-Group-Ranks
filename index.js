94908dd282bb280098749de7d39cfcc07826b562
Rafalmilevski <168198043+Rafalmilevski@users.noreply.github.com>
2026-05-08 18:19:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("API is working");
-});
-
-app.get("/rank", (req, res) => {
-    res.json({
-        player: "Test",
-        rank: 99
-    });
-});
-
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-    console.log("Running on port " + port);
+    console.log("Server running");
 });
